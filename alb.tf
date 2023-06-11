38cdeba5c2efa4493d80604cb6c98df9de961c45
Damian Sandoval <will.ddr@gmail.com>
2023-06-11 13:23:22 -0300

    retoques en el ALB

--- alb.tf
+++ alb.tf
@@ -1,3 +1,18 @@
+# Creamos target group
+
+resource "aws_lb_target_group" "oblimanual-trg" {
+  name        = "oblimanual-trg"
+  port        = 80
+  target_type = "instance"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.oblimanual.id
+}
+
+resource "aws_alb_target_group_attachment" "oblimanual-tg-aws_alb_target_group_attach" {
+  target_group_arn = aws_lb_target_group.oblimanual-trg.arn
+  target_id        = aws_instance.oblimanual-inst1.id
+}
+
 resource "aws_lb" "obli-alb" {
   name               = "oblimanual-alb"
   internal           = false
@@ -5,13 +20,17 @@ resource "aws_lb" "obli-alb" {
   security_groups    = [aws_security_group.oblimanual-sg.id]
   subnets            = [aws_subnet.oblimanual-subnet1-publica.id, aws_subnet.oblimanual-subnet2-publica.id]
   # depends_on         = [aws_vpc.aws_vpc.oblimanual] # Se agrega dependencia para que el alb se cree luego del vpc
+  enable_deletion_protection = false
+  tags = {
+    Environment = "prod"
+  }
 }
 
 # Creamos Listener
 
 resource "aws_lb_listener" "oblimanual-listener" {
   load_balancer_arn = aws_lb.obli-alb.arn
-  port              = 80
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
@@ -20,12 +39,19 @@ resource "aws_lb_listener" "oblimanual-listener" {
   }
 }
 
-# Creamos target group
+### creamos la regla para el listener
+resource "aws_lb_listener_rule" "oblimanual-listener-rule" {
+  listener_arn = aws_lb_listener.oblimanual-listener.arn
+  priority     = 100
 
-resource "aws_lb_target_group" "oblimanual-trg" {
-  name     = "oblimanual-trg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.oblimanual.id
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oblimanual-trg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/var/www/html/index.html"]
+    }
+  }
 }
-
--- instances.tf
+++ instances.tf
@@ -7,7 +7,26 @@ resource "aws_instance" "oblimanual-inst1" {
   subnet_id              = aws_subnet.oblimanual-subnet1-publica.id
   vpc_security_group_ids = [aws_security_group.oblimanual-sg.id]
   tags = {
-    Name = "oblimanual-inst1"
+    Name      = "oblimanual-inst1"
+    terraform = "True"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("~/Descargas/vockey.pem")
+  }
+
+  # Dejamos prontos los servicio de git y httpd
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y httpd git",              # -y lo que hace es no pedir confirmacion, instalo httpd y git
+      "sudo systemctl start httpd",                 # arranco el servicio httpd
+      "sudo systemctl start httpd",                 # dejo habilitado el servicio httpd
+      "sudo touch /var/www/html/index.html",        #creo el index.html vacio
+      "sudo echo 'nodo1' > /var/ww/html/index.html" #hago un echo con el nombre del nodo y se lo pongo en el archivo
+    ]
   }
 }
 
@@ -18,28 +37,26 @@ resource "aws_instance" "oblimanual-inst2" {
   subnet_id              = aws_subnet.oblimanual-subnet2-publica.id
   vpc_security_group_ids = [aws_security_group.oblimanual-sg.id]
   tags = {
-    Name = "oblimanual-inst2"
+    Name      = "oblimanual-inst2"
+    terraform = "True"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("~/Descargas/vockey.pem")
+  }
+
+  # Dejamos prontos los servicio de git y httpd
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y httpd git",              # -y lo que hace es no pedir confirmacion, instalo httpd y git
+      "sudo systemctl start httpd",                 # arranco el servicio httpd
+      "sudo systemctl start httpd",                 # dejo habilitado el servicio httpd
+      "sudo touch /var/www/html/index.html",        #creo el index.html vacio
+      "sudo echo 'nodo2' > /var/ww/html/index.html" #hago un echo con el nombre del nodo y se lo pongo en el archivo
+    ]
   }
 }
-
-
-/*
-connection {
-  type        = "ssh"
-  user        = "ec2-user"
-  host        = self.public_ip
-  private_key = file("/Users/maikool/downloads/vockey.pem")
-}
-
-# Dejamos prontos los servicio de git y httpd
-
-provisioner "remote-exec" {
-  inline = [
-    "sudo yum install -y httpd git", # -y lo que hace es no pedir confirmacion, instalo httpd y git
-    "sudo systemctl start httpd",    # arranco el servicio httpd
-    "sudo systemctl start httpd"     # dejo habilitado el servicio httpd
-
-  ]
-}
-
-*/
