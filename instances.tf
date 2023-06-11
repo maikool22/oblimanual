2c4fc52bfb9c82203d92f967264b61ce7a166d87
maikool <maikool22@me.com>
2023-06-10 21:07:05 -0300

    creo instancias y agrego provisioner

--- instances.tf
+++ instances.tf
@@ -1,1 +1,31 @@
-# ami-04a0ae173da5807d3
+# Creamos dos intancias
+
+resource "aws_instance" "oblimanual-inst1" {
+  ami                    = var.ami-id
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+  vpc_security_group_ids = [aws_security_group.oblimanual-sg]
+}
+
+resource "aws_instance" "oblimanual-inst2" {
+  ami                    = var.ami-id
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+  vpc_security_group_ids = [aws_security_group.oblimanual-sg]
+}
+
+connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("/Users/maikool/downloads/vockey.pem")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y httpd git",  # -y lo que hace es no pedir confirmacion, instalo httpd y git
+      "sudo systemctl start httpd",     # arranco el servicio httpd
+      "sudo systemctl start httpd"      # dejo habilitado el servicio httpd
+      
+    ]
+  }
